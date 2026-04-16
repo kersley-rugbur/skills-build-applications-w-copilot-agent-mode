e67ec966f2ad2e64e1e501ffb05705795a008aad
kersley-rugbur <kersley.rugbur@accenture.com>
2026-04-16 18:17:28 +0000

    Update API URLs to use silver-halibut-g4p54655765939gw9 and secure ALLOWED_HOSTS for Codespaces and localhost

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,13 @@ SECRET_KEY = 'django-insecure--gf%hu9$fxi0nmq&-!ssay)eik4v!$5tfha+yz!7_yxn)udn!q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+import os
+# Allow localhost and Codespace public URL
+codespace_name = os.environ.get('CODESPACE_NAME')
+allowed_hosts = ['localhost', '127.0.0.1']
+if codespace_name:
+    allowed_hosts.append(f'{codespace_name}-8000.app.github.dev')
+ALLOWED_HOSTS = allowed_hosts
 
 
 # Application definition
